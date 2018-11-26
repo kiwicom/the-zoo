e0315de2db42175007d13155f6b52142b00f7cfb
Bence Nagy <bence@underyx.me>
2018-11-26 18:45:33 +0100

    check_python_lib_licenses, Handle multiple licenses

--- zoo/analytics/tasks/licenses.py
+++ zoo/analytics/tasks/licenses.py
@@ -10,20 +10,26 @@ def check_python_lib_licenses():
     session = requests.Session()
     for dependency in Dependency.objects.filter(
         type=DependencyType.PY_LIB.value, license=None
-    ).only("pk", "name"):
+    ).only("name"):
         try:
             response = session.get(f"https://pypi.org/pypi/{dependency.name}/json")
             response.raise_for_status()
             try:
-                dependency.license = next(
-                    classifier.replace("License :: ", "").replace(
-                        "OSI Approved :: ", ""
-                    )
+                licenses = {
+                    classifier.replace("License :: ", "")
+                    .replace("OSI Approved", "")
+                    .strip(" :")
                     for classifier in response.json()["info"]["classifiers"]
                     if classifier.startswith("License :: ")
+                }
+                dependency.license = ", ".join(
+                    license for license in licenses if license
                 )
             except StopIteration:
                 dependency.license = "No license"  # choosealicense.com/no-permission
             dependency.save()
-        except requests.RequestException:
+        except requests.RequestException as ex:
+            if ex.response.status_code == 404:
+                continue
+
             client.captureException()

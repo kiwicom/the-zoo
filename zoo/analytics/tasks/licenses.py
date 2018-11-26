5e3a706ad39b6f1fbaab75578058ad5bc7658a24
Bence Nagy <bence@underyx.me>
2018-11-26 18:06:41 +0100

    check_python_lib_licenses: Fix database query

--- zoo/analytics/tasks/licenses.py
+++ zoo/analytics/tasks/licenses.py
@@ -9,19 +9,21 @@ from ..models import Dependency, DependencyType
 def check_python_lib_licenses():
     session = requests.Session()
     for dependency in Dependency.objects.filter(
-        type=DependencyType.PY_LIB, license=None
-    ).values("name"):
+        type=DependencyType.PY_LIB.value, license=None
+    ).only("pk", "name"):
         try:
             response = session.get(f"https://pypi.org/pypi/{dependency.name}/json")
             response.raise_for_status()
             try:
                 dependency.license = next(
-                    classifier.replace("License :: ").replace("OSI Approved :: ")
+                    classifier.replace("License :: ", "").replace(
+                        "OSI Approved :: ", ""
+                    )
                     for classifier in response.json()["info"]["classifiers"]
                     if classifier.startswith("License :: ")
                 )
             except StopIteration:
                 dependency.license = "No license"  # choosealicense.com/no-permission
             dependency.save()
-        except requests.RequestError:
+        except requests.RequestException:
             client.captureException()

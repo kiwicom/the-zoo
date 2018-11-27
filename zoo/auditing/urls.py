3ae5f01dc0df55db0cd350914c9acf2a82f808d1
Alex Viscreanu <alexandru.viscreanu@kiwi.com>
2018-11-27 15:00:29 +0100

    Refactor URL structure for paths depending on the service

--- zoo/auditing/urls.py
+++ zoo/auditing/urls.py
@@ -1,9 +1,9 @@
 """services URL Configuration."""
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
-urlpatterns = [
+global_urls = [
     path("", views.AuditOverview.as_view(), name="audit_overview"),
     path("bulk_create/", views.open_bulk_git_issues, name="bulk_create_issues"),
     path(
@@ -11,19 +11,18 @@ urlpatterns = [
         views.AuditOverview.as_view(),
         name="owned_audit_overview",
     ),
+]
+
+service_urls = [
+    path("", views.AuditReport.as_view(), name="audit_report"),
+    path("<int:issue_pk>/gitlab_issue", views.open_git_issue, name="open_git_issue"),
+    path("<int:issue_pk>/wontfix", views.wontfix_issue, name="wontfix_issue"),
+]
+
+urlpatterns = [
+    path("auditing/", include(global_urls)),
     path(
-        "<str:service_owner_slug>/<str:service_name_slug>",
-        views.AuditReport.as_view(),
-        name="audit_report",
-    ),
-    path(
-        "<str:service_owner_slug>/<str:service_name_slug>/issues/<int:issue_pk>/gitlab_issue",
-        views.open_git_issue,
-        name="open_git_issue",
-    ),
-    path(
-        "<str:service_owner_slug>/<str:service_name_slug>/issues/<int:issue_pk>/wontfix",
-        views.wontfix_issue,
-        name="wontfix_issue",
+        "services/<str:service_owner_slug>/<str:service_name_slug>/auditing",
+        include(service_urls),
     ),
 ]

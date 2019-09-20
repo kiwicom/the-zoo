dcc51aecf7e236ed5530cb8f500ed56eae84b19e
Pavel Dedik <pavel.dedik@kiwi.com>
2019-09-20 14:09:43 +0200

    vue: Fix broken repo-input component

--- zoo/base/assets/js/components/RepoInputSuggestions.vue
+++ zoo/base/assets/js/components/RepoInputSuggestions.vue
@@ -1,56 +1,94 @@
 <template>
-    <div class="menu transition visible"
-        v-if="visible"
-        @keydown.up="increaseCounter"
-        v-on:click="selectSuggestion"
-        @mouseover="setCursor"
+  <div class="menu transition visible"
+    v-if="visible"
+    @keydown.up="increaseCounter"
+    @mouseover="setCursor"
+    @mouseleave="resetCursor"
+  >
+    <div
+      :index="i" :key="i"
+      v-for="(suggestion, i) in visibleSuggestions"
+      :class="{'selected': i === cursor, 'item': true}"
     >
-        <div :index="i" v-for="(suggestion, i) in suggestions" :key="suggestion.reference" :class="{'selected': i === cursor, 'item': true}">
-        <span>{{ suggestion.reference }} </span>
-        </div>
-        <div class="item" style="text-align: center;" v-if="count === 0">
-        <span>
-            No results found
-        </span>
-        </div>
-    </ul>
+      <span>{{ suggestion.reference }} </span>
+    </div>
+    <div class="item" style="text-align: center;" v-if="count === 0">
+    <span>
+        No results found
+    </span>
+    </div>
   </div>
 </template>
 
 <script>
+import * as R from "ramda"
+
 export default {
-  data () {
-    return {
-      cursor: 0
-    };
-  },
   template: "#repo-input-suggestions-markup",
   props: ["inputHasFocus"],
-  computed: {
+  data () {
+    return {
+      cursor: -1,
+      cursorMax: 0,
+    };
+  },
+  watch: {
     suggestions () {
-      this.cursor = 0;
+      this.cursor = -1;
+    },
+  },
+  computed: {
+    showCount () {
+      if (this.cursor > this.cursorMax) {
+        this.cursorMax = this.cursor;
+      }
+      return this.cursorMax + 10;
+    },
+    suggestions () {
       return this.$store.getters.getFilteredSuggestions;
+    },
+    visibleSuggestions () {
+      return R.take(this.showCount, this.suggestions);
     },
     count () {
       return this.suggestions.length;
     },
     visible () {
-      return this.$store.state.enteredText.length > 0 && this.inputHasFocus;
-    }
+      return this.inputHasFocus;
+    },
   },
   methods: {
     selectSuggestion () {
-      if (this.visible) {
+      if (this.visible && this.count > 0 && this.cursor >= 0) {
         this.$store.commit(
           "selectSuggestion",
-          this.suggestions[this.cursor].id
+          this.suggestions[this.cursor].id,
         );
-        document.activeElement.blur();
+      } else {
+        console.log(this.cursor);
+        this.$store.commit(
+          "selectSuggestion",
+          "",
+        );
       }
     },
+    blurActiveElement () {
+      document.activeElement.blur();
+    },
     setCursor: function(event) {
-      if (this.visible)
-        this.cursor = parseInt(event.target.getAttribute("index"));
+      if (this.visible) {
+        if (event.target.hasAttribute("index")) {
+          var element = event.target;
+        } else if (event.target.closest("div.selected")) {
+          var element = event.target.closest("div.selected");
+        }
+        if (element !== undefined) {
+          this.cursor = parseInt(element.getAttribute("index"));
+        }
+      }
+    },
+    resetCursor () {
+      this.cursor = -1;
     },
     goUp () {
       if (this.visible) this.cursor = Math.max(this.cursor - 1, 0);
@@ -58,10 +96,10 @@ export default {
     goDown () {
       if (this.visible)
         this.cursor = Math.min(this.cursor + 1, this.suggestions.length - 1);
-    }
+    },
   },
   created () {
-    this.$parent.$on("select", this.selectSuggestion);
+    this.$parent.$on("select", this.blurActiveElement);
     this.$parent.$on("decreaseCursor", this.goUp);
     this.$parent.$on("increaseCursor", this.goDown);
   }

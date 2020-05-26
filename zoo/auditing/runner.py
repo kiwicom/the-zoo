b49ad534a9f2d86015238fac87b4beb513c7fb2f
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-05-26 13:13:39 +0200

    feat: Add notification hook

--- zoo/auditing/runner.py
+++ zoo/auditing/runner.py
@@ -2,12 +2,18 @@ from collections import namedtuple
 
 import arrow
 import structlog
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.urls import reverse
 from raven.contrib.django.raven_compat.models import client
+from slacker import Error as SlackError
+from slacker import Slacker
 
 from .check_discovery import CHECKS
 from .models import Issue
 
 log = structlog.get_logger()
+slack = Slacker(settings.SLACK_TOKEN)
 
 Result = namedtuple("Result", ["issue_key", "is_found", "details"], defaults=(None,))
 CodePatch = namedtuple(
@@ -58,13 +64,36 @@ def create_issue(issue_repo, issue_key, is_found, details=None):
     )
 
 
-def update_issue(issue, is_found, details=None):
+def update_issue(issue: Issue, is_found, details=None):
     if issue.details != details:
         issue.details = details
     issue.status = determine_issue_status(is_found, issue.status)
     issue.last_check = arrow.utcnow().datetime
     issue.deleted = False
     issue.save()
+
+    notify_status_change(issue)
+
+
+def notify_status_change(issue: Issue):
+    if Issue.Status(issue.status) in [Issue.Status.NEW, Issue.Status.REOPENED]:
+        site = Site.objects.get_current()
+        audit_url = reverse(
+            "audit_report",
+            args=("services", issue.repository.owner, issue.repository.name),
+        )
+        text = "{status} issue {issue} on <{repo.url}|{repo.name}>.".format(
+            status=issue.status.title(),
+            issue=f"<http://{site.domain}{audit_url}|{issue.kind.title}>",
+            repo=issue.repository,
+        )
+        for channel in issue.repository.services.values_list(
+            "slack_channel", flat=True
+        ):
+            try:
+                slack.chat.post_message(channel, text)
+            except SlackError as error:
+                log.exception("auditing.update_issue.slack_error", error=repr(error))
 
 
 def save_check_result(issue_repo, issue_key, is_found, details=None):

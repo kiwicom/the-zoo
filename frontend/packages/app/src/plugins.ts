b064c5c22150e782a4d078972a83bcb0beca9044
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-08-24 14:57:26 +0200

    feat: Added Service plugin

--- frontend/packages/app/src/plugins.ts
+++ frontend/packages/app/src/plugins.ts
@@ -7,3 +7,5 @@ export { plugin as Circleci } from '@backstage/plugin-circleci';
 export { plugin as LighthousePlugin } from '@backstage/plugin-lighthouse';
 export { plugin as TechRadar } from '@backstage/plugin-tech-radar';
 export { plugin as GithubActions } from '@backstage/plugin-github-actions';
+
+export { plugin as Services } from '@backstage/plugin-services';

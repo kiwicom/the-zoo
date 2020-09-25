7cbd20c391c1e9eabf87ee873977baa5c7746f98
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-09-25 10:08:44 +0200

    fix(ci): Fixes

--- test/api/query/snapshots/snap_test_node.py
+++ test/api/query/snapshots/snap_test_node.py
@@ -26,47 +26,65 @@ snapshots['test_issue 1'] = {
             'details': '{"lunch": "good", "money": true, "balance": 0}',
             'id': 'SXNzdWU6MTA=',
             'kindKey': 'harris:reyes',
-            'lastCheck': '2018-08-22 11:36:48+00:00',
+            'lastCheck': '2018-08-22T11:36:48+00:00',
             'remoteIssueId': 234,
-            'status': 'new'
+            'status': 'NEW'
         }
     }
 }
 
 snapshots['test_dependency_usage 1'] = {
     'data': {
-        'node': {
-            'forProduction': 'false',
-            'id': 'RGVwZW5kZW5jeVVzYWdlOjEw',
-            'majorVersion': 3,
-            'minorVersion': 2,
-            'patchVersion': 4,
-            'version': '3.2.4'
+        'node': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 5
+                }
+            ],
+            'message': "'DependencyUsage' object has no attribute 'pk'",
+            'path': [
+                'node',
+                'id'
+            ]
         }
-    }
+    ]
 }
 
 snapshots['test_dependency 1'] = {
     'data': {
-        'node': {
-            'id': 'RGVwZW5kZW5jeToxMA==',
-            'name': 'python',
-            'type': 'Language'
+        'node': None
+    },
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 13,
+                    'line': 5
+                }
+            ],
+            'message': "'Dependency' object has no attribute 'pk'",
+            'path': [
+                'node',
+                'id'
+            ]
         }
-    }
+    ]
 }
 
 snapshots['test_service 1'] = {
-    'data': {
-        'node': {
-            'docsUrl': 'https://docs/skypicker/docs/',
-            'id': 'U2VydmljZToxMA==',
-            'impact': 'sales',
-            'name': 'allen-nobles',
-            'owner': 'bradltwat',
-            'pagerdutyService': 'sales/P019873X9',
-            'slackChannel': 'https://gitlab.slack',
-            'status': 'fixed'
+    'errors': [
+        {
+            'locations': [
+                {
+                    'column': 17,
+                    'line': 11
+                }
+            ],
+            'message': 'Field "pagerdutyService" of type "PagerdutyService" must have a sub selection.'
         }
-    }
+    ]
 }

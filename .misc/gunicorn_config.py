bceff404b7c9a6e7d27230c49de3ccb82bb1e9f4
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-10-15 12:32:29 +0200

    feat: Ignore kube-probe for logging

--- .misc/gunicorn_config.py
+++ .misc/gunicorn_config.py
@@ -1,7 +1,27 @@
 """Link to docs: http://docs.gunicorn.org/en/stable/settings.html."""
 
+from gunicorn.glogging import Logger
+
+
+class CustomLogger(Logger):
+    def access(self, resp, req, environ, request_time):
+        """Do not log if User Agent is present within the ignore list."""
+        ignore_user_agents = ["kube-probe"]
+
+        headers = {name.lower(): value for name, value in req.headers}
+        request_user_agent = headers.get("user-agent", "")
+
+        for agent in ignore_user_agents:
+            if request_user_agent.startswith(agent):
+                return
+
+        super().access(resp, req, environ, request_time)
+
+
 worker_class = "gevent"
 
 bind = ":8080"
 
 accesslog = "-"  # send access log to stdout
+
+logger_class = CustomLogger

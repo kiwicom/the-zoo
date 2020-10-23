89e7434bc0e96884132edf7f0375d80c909fbedc
Baptiste Darthenay <baptiste.darthenay@kiwi.com>
2020-10-23 15:35:19 +0200

    feat(backstage): Add Backstage with create-app alpha.25

---
+++ frontend/packages/backend/src/plugins/catalog.ts
@@ -0,0 +1,44 @@
+import {
+  createRouter,
+  DatabaseEntitiesCatalog,
+  DatabaseLocationsCatalog,
+  DatabaseManager,
+  HigherOrderOperations,
+  LocationReaders,
+  runPeriodically,
+} from '@backstage/plugin-catalog-backend';
+import { PluginEnvironment } from '../types';
+import { useHotCleanup } from '@backstage/backend-common';
+
+export default async function createPlugin({
+  logger,
+  config,
+  reader,
+  database,
+}: PluginEnvironment) {
+  const locationReader = new LocationReaders({ logger, reader, config });
+
+  const db = await DatabaseManager.createDatabase(await database.getClient(),
+    { logger },
+  );
+  const entitiesCatalog = new DatabaseEntitiesCatalog(db);
+  const locationsCatalog = new DatabaseLocationsCatalog(db);
+  const higherOrderOperation = new HigherOrderOperations(
+    entitiesCatalog,
+    locationsCatalog,
+    locationReader,
+    logger,
+  );
+
+  useHotCleanup(
+    module,
+    runPeriodically(() => higherOrderOperation.refreshAllLocations(), 10000),
+  );
+
+  return await createRouter({
+    entitiesCatalog,
+    locationsCatalog,
+    higherOrderOperation,
+    logger,
+  });
+}

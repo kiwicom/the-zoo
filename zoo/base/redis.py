a6560ff27e7aa2de7933751f9e64b2d82f042056
Jaroslav Sevcik <jaroslav.sevcik@kiwi.com>
2021-02-05 12:13:55 +0100

    feat(redis): add retry mechanism for redis connection

--- zoo/base/redis.py
+++ zoo/base/redis.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from redis import StrictRedis
+from tenacity import retry, stop_after_attempt, wait_exponential
 
 
+@retry(wait=wait_exponential(multiplier=1, min=1, max=5), stop=stop_after_attempt(5))
 def get_connection():
     return StrictRedis.from_url(settings.REDIS_CACHE_URL)

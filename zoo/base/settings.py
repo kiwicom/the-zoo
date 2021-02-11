bb67233a91b09ae808ef65c12ce7c1b78b4fb32c
Ivica Munitic <ivica.munitic@kiwi.com>
2021-02-11 10:48:53 +0100

    feat: if hangar is not used add the api token auth middleware

--- zoo/base/settings.py
+++ zoo/base/settings.py
@@ -21,6 +21,7 @@ version = _get_app_version()
 root = Path(__file__).parents[1]
 env = environ.Env(
     ZOO_DEBUG=(bool, False),
+    ZOO_USE_HANGAR=(bool, False),
     ZOO_CELERY_BROKER_URL=(str, "redis://redis/0"),
     ZOO_REDIS_CACHE_URL=(str, "redis://redis/1"),
     ZOO_REDBEAT_REDIS_URL=(str, "redis://redis/2"),
@@ -152,6 +153,10 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+USE_HANGAR = env("ZOO_USE_HANGAR")
+if USE_HANGAR:
+    MIDDLEWARE.append("zoo.api.middleware.ApiTokenAuthenticationMiddleware")
 
 ROOT_URLCONF = "zoo.base.urls"
 
